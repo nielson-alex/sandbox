a447385a3fb2c8975225bdbe10be7770930ed39b
Alex Nielson <alexnielson90@gmail.com>
2017-03-03 15:38:16 -0800

    (03/03/2017) <Alex Nielson> Successfully made it so the ZhongwenGreetingView class successfully loops rather than immediately quitting out of the view upon completing the function

--- sandbox/src/sandbox/view/ZhongwenGreetingView.java
+++ sandbox/src/sandbox/view/ZhongwenGreetingView.java
@@ -12,18 +12,50 @@ import java.util.Scanner;
  * @author Alex
  */
 public class ZhongwenGreetingView {
-    
+    private String menu;
+    //private String promptMessage;
+
+    public ZhongwenGreetingView() {
+        this.menu = "\n你好！这儿有联系中文的机会。开始以前，请告诉我你叫什么姓名?\n\n"
+                + "（为了取消，请打‘Q’）";
+    }
+
     public void displayZhongwenGreetingView() {
-    //prompt to enter amount of USD to convert to Euros
-        System.out.println("\n你好！你叫什么名字？ ");
+        boolean done = false;
 
-        // create an input file for the console
-        Scanner inFile;
-        inFile = new Scanner(System.in);
+        do {
+            String chineseName = this.getChineseName();
+            if (chineseName.toUpperCase().equals("Q")) {
+                return;
+            }
+            done = this.doAction(chineseName);
+        } while (!done);
+    }
 
-        // read the value of the next double typed into the console
-        String name = inFile.nextLine();
-        
-        System.out.println("\n" + name + "我很高兴认识你！");
+    String getChineseName() {
+        Scanner keyboard = new Scanner(System.in); // get infile for keyboard 
+        String value = ""; //value to be returned
+        boolean valid = false; // initialize to not valid
+
+        while (!valid) {
+            System.out.println("\n" + this.menu);
+
+            value = keyboard.nextLine();
+
+            if (value.length() < 1) {
+                System.out.println("\nTry again. Name cannot be"
+                        + "\nless than one characer.");
+                continue;
+            }
+            break;
+        }
+
+        return value;
+    }
+
+    boolean doAction(String chineseName) {//prompt to greet user in Chinese and get their name
+        System.out.println("\n" + chineseName + "我很高兴认识你！");
+
+        return false;
     }
 }
